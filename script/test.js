677a01f70a4cbc938533680fe22ea998bdfe6cba
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-02-21 11:25:30 +0100

    randQuestion

--- script/test.js
+++ script/test.js
@@ -1,24 +1,51 @@
 console.log('Hello World!')
 //costruttore domande (Michele suggerisce classe)
-let questions = JSON.stringify(new URL("questions.json"))
-console.log(questions)
+let questions = [{
+    question: "Qual'è la pizza preferita di Michele?",
+    rightAnswer: "Diavola",
+    wrongAnswers:["Margherita","Hawaiana", "Capricciosa"]
+},
+{
+    question: "What does CPU stand for?",
+    rightAnswer: "Central Processing Unit",
+    wrongAnswers:["Central Process Unit","Computer Personal Unit","Central Processor Unit"]
+},
+{
+    question: "In web design, what does CSS stand for?",
+    rightAnswer: "Cascading Style Sheet",
+    wrongAnswers:[ "Counter Strike: Source",
+    "Corrective Style Sheet",
+    "Computer Style Sheet"]
+},
+{
+    question: "On Twitter, what is the character limit for a Tweet?",
+    rightAnswer: "140",
+    wrongAnswers:["120", "160", "100"]
+},
+{
+    question: "Which programming language shares its name with an island in Indonesia?",
+    rightAnswer: "Java",
+    wrongAnswers:["Python", "C", "Jakarta"]
+},
+{
+    question: "What is the code name for the mobile operating system Android 7.0?",
+    rightAnswer: "Nougat",
+    wrongAnswers:["Ice Cream Sandwich",
+    "Jelly Bean",
+    "Marshmallow"]
+}]
+console.log(questions[3])
+
+
 
 class QuestionMarkI{
-    constructor(q,rAns,wAns){
+    /*constructor(q,rAns,wAns){
         this.question= q;
         this.rightAns = rAns;
         this.wrongAns = wAns;
-    }
+    }*/
 
     
-    
-    shuffle(){
-        let box = document.querySelector('.question')
-        
-        
-        
-        //questions.sort => math.random
-    }
 
     pickQuestion(){
 
@@ -29,7 +56,18 @@ class QuestionMarkI{
     }
 
 }
+let indiceCasuale = questions[Math.floor(Math.random()*questions.length)]
+console.log(indiceCasuale)
+let randQ = indiceCasuale.question
+console.log(randQ)
+let qPick = document.querySelector('question').innerHTML = randQ
 
+
+       // let domandaCasuale = questions.forEach(i => i.question = Math.floor(Math.random(i.question)))
+        
+
+
+QuestionMarkI.shuffleAndPick()
 //timer
 
 function timer(){
