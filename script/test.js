521abde20ac09f00e054f6ed305e28c0ce059a40
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-02-21 10:12:39 +0100

    jsClass

--- script/test.js
+++ script/test.js
@@ -1,1 +1,39 @@
 console.log('Hello World!')
+//costruttore domande (Michele suggerisce classe)
+let questions = JSON.stringify(new URL("questions.json"))
+console.log(questions)
+
+class QuestionMarkI{
+    constructor(q,rAns,wAns){
+        this.question= q;
+        this.rightAns = rAns;
+        this.wrongAns = wAns;
+    }
+
+    
+    
+    shuffle(){
+        let box = document.querySelector('.question')
+        
+        
+        
+        //questions.sort => math.random
+    }
+
+    pickQuestion(){
+
+    }
+
+    validate(){
+
+    }
+
+}
+
+//timer
+
+function timer(){
+    
+}
+
+//counter domande
