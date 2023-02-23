1b40f8333fa3d1667f4a2139d3c53d05d24fc0b3
Marianna <mari.albrici@me.com>
2023-02-23 13:41:11 +0100

    add JS Mari

--- script/provaaaaaa.js
+++ script/provaaaaaa.js
@@ -139,3 +139,50 @@ answerButtons.forEach((button, index) => {
 }
 
 });
+
+
+//CERCHIO PERCENTUALE RISPOSTE MARI - SE FUNZIONASSE...
+
+var circle = document.querySelector('circle');
+var radius = circle.r.baseVal.value;
+var circumference = radius * 2 * Math.PI;
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = `${circumference}`;
+
+function setProgress(percent) {
+    const offset = circumference - percent / 100 * circumference;
+    circle.style.strokeDashoffset = offset;
+}
+
+let inputPercentuale = document.querySelector('input');
+setProgress(inputPercentuale.value);
+
+inputPercentuale.addEventListener('change', function (e) {
+    if (inputPercentuale.value < 101 && input.value > 0) {
+        setProgress(input.value);
+    }
+})
+
+
+let topMessage = document.getElementById('topMessage');
+let middleMessage = document.getElementById('middleMessage');
+let underMessage = document.getElementById('underMessage');
+
+inputPercentuale.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        let percentage = Number(input.value);
+        function message() {
+            if (percentage >= 60) {
+                topMessage.innerText = 'Congratulations!';
+                middleMessage.innerText = 'You passed the exam.';
+                underMessage.innerText = 'We\'ll send you the certificate in a few minutes. Check your email (including the promotions/spam folder)'
+            } else {
+                topMessage.innerText = 'We\'re sorry';
+                middleMessage.innerText = 'You did not pass the exam.';
+                underMessage.innerText = 'Check your email (including the promotions/spam folder)';
+            }
+        }
+        message();
+    }
+})
