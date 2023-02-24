93b83ab71de5f90f9f00ffe7153a15053414f45f
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-02-24 15:40:13 +0100

    formattazione 0.9

--- script/provaaaaaa.js
+++ script/provaaaaaa.js
@@ -1,33 +1,4 @@
-let correctAnswers = 0;
-let wrongAnswers = 0;
-
-
-let input = document.querySelector('input');
-let array1 = [];
-
-localStorage.getItem('items', JSON.stringify(array1));
-
-let bottone = document.getElementById('btn-to-welcome')
-
-bottone.addEventListener('click', function salvare() {
-    array1.push(input.value);
-    localStorage.setItem('items', JSON.stringify(array1));    
-});
-//localStorage.clear();
-function stellacoloratorMarkII() {
-  let stars = document.querySelectorAll("path");
-  stars.forEach((star, index1) => {
-    star.addEventListener("click", () => {
-      stars.forEach((star, index2) => {
-        index1 >= index2
-          ? star.classList.add("active")
-          : star.classList.remove("active");
-      });
-    });
-  });
-}
-stellacoloratorMarkII();
-
+//DOMANDE DEL TEST
 let questions = [
   {
     question: "Qual è il tuo nome?",
@@ -91,12 +62,47 @@ let questions = [
   },
 ];
 
+//VARIABILI GLOBALI
+let correctAnswers = 0;
+let wrongAnswers = 0;
+let input = document.querySelector("input");
+let array1 = [];
+let bottone = document.getElementById("btn-to-welcome");
+let questionElement = document.getElementById("question");
+let answerButtons = document.querySelectorAll(".answer-buttons");
+let counterElement = document.getElementById("counter");
+let nextQuestionButton = document.getElementById("il-bottone-dei-bottoni");
+let counter = 0;
+
+//SALVATAGGIO INPUT IN LOCAL STORAGE
+localStorage.getItem("items", JSON.stringify(array1));
+bottone.addEventListener("click", function salvare() {
+  array1.push(input.value);
+  localStorage.setItem("items", JSON.stringify(array1));
+});
+//localStorage.clear(); SERVE A PULIRE IL LOCAL STORAGE
+
+//SISTEMA VALUTAZIONE A STELLE
+function stellacoloratorMarkII() {
+  let stars = document.querySelectorAll("path");
+  stars.forEach((star, index1) => {
+    star.addEventListener("click", () => {
+      stars.forEach((star, index2) => {
+        index1 >= index2
+          ? star.classList.add("active")
+          : star.classList.remove("active");
+      });
+    });
+  });
+}
+stellacoloratorMarkII();
+
 // Nascondi tutti gli elementi tranne l'header
 document.getElementById("matteo").style.display = "none";
 document.getElementById("andrea").style.display = "none";
 document.getElementById("marianna").style.display = "none";
 
-// Aggiungi un evento al pulsante "Procedi" dell'header per mostrare solo il main con id "matteo"
+// DISPLAY FIRST PAGE
 document
   .getElementById("btn-to-welcome")
   .addEventListener("click", function () {
@@ -106,7 +112,7 @@ document
     document.getElementById("marianna").style.display = "none";
     document.getElementById("matteo").style.display = "block";
   });
-
+//DISPLAY SECOND PAGE
 document.getElementById("btn-to-test").addEventListener("click", function () {
   document.getElementById("emanuele").style.display = "none";
   document.getElementById("andrea").style.display = "block";
@@ -115,63 +121,50 @@ document.getElementById("btn-to-test").addEventListener("click", function () {
 });
 
 //TIMER
-
-
-
 let timeLeft = 40;
-let rolex = document.querySelector('.rolex')
+let rolex = document.querySelector(".rolex");
 let rolexTimer;
 
-function startTime(){
-    
-    if(rolexTimer){
-        clearInterval(rolexTimer);
-        timeLeft = 40;
-    }
-    rolexTimer =  setInterval(function () {
-    if(document.getElementById("andrea").style.display == "block"){
-        if (timeLeft == 0 ) {
+function startTime() {
+  if (rolexTimer) {
+    clearInterval(rolexTimer);
+    timeLeft = 40;
+  }
+  rolexTimer = setInterval(function () {
+    if (document.getElementById("andrea").style.display == "block") {
+      if (timeLeft == 0) {
         counter++;
         counterElement.innerHTML = "Domanda :" + counter;
         clearInterval(rolexTimer);
         newQuestion();
-        } else {
+      } else {
         timeLeft--;
-        document.querySelector(".rolex").innerHTML = timeLeft + " seconds remaining ";
-        }
+        document.querySelector(".rolex").innerHTML =
+          timeLeft + " seconds remaining ";
+      }
     }
-}, 1000)
+  }, 1000);
 }
-
 startTime();
 
-
-
+//GENERA UNA NUOVA DOMANDA CASUALE
 let newQuestion = function () {
-    startTime();
-    let randomQuestion = questions[Math.floor(Math.random() * questions.length)];
-    questionElement.innerText = randomQuestion.question;
-    questions.splice(
-    questions.findIndex((i) => i.question === randomQuestion.question),1);
+  startTime();
+  let randomQuestion = questions[Math.floor(Math.random() * questions.length)];
+  questionElement.innerText = randomQuestion.question;
+  questions.splice(
+    questions.findIndex((i) => i.question === randomQuestion.question),
+    1
+  );
 
-    let answers = [...randomQuestion.wrongAnswers, randomQuestion.rightAnswer];
-    answerButtons.forEach((button, index) => {
+  let answers = [...randomQuestion.wrongAnswers, randomQuestion.rightAnswer];
+  answerButtons.forEach((button, index) => {
     button.innerText = answers[index];
     button.dataset.correct = answers[index] === randomQuestion.rightAnswer;
-
-    });
+  });
 };
 
-
-// Seleziona gli elementi HTML che devono essere aggiornati
-let questionElement = document.getElementById("question");
-let answerButtons = document.querySelectorAll(".answer-buttons");
-let counterElement = document.getElementById("counter");
-
-let nextQuestionButton = document.getElementById("il-bottone-dei-bottoni");
-
-let counter = 0;
-
+//RACCOLTA DATI DOMANDE
 answerButtons.forEach((button) => {
   button.addEventListener("click", function () {
     if (button.dataset.correct === "true") {
@@ -184,32 +177,29 @@ answerButtons.forEach((button) => {
   });
 });
 
-
-answerButtons.forEach(function(button) {
+//FUNZIONAMENTO A SCHERMO DEI BOTTONI
+answerButtons.forEach(function (button) {
   button.addEventListener("click", function () {
     // Rimuovi la classe 'selected' da tutti gli altri bottoni
-    answerButtons.forEach(function(otherButton) {
+    answerButtons.forEach(function (otherButton) {
       if (otherButton !== button) {
-        otherButton.classList.remove("selected")
+        otherButton.classList.remove("selected");
       }
     });
-
     // Aggiungi o rimuovi la classe 'selected' dal bottone cliccato
     if (button.classList.contains("selected")) {
-      button.classList.remove("selected")
+      button.classList.remove("selected");
     } else {
-      button.classList.add("selected")
+      button.classList.add("selected");
     }
   });
 });
-
 nextQuestionButton.addEventListener("click", function () {
   // Rimuovi la classe 'selected' da tutti i bottoni
-  answerButtons.forEach(function(button) {
-    button.classList.remove("selected")
+  answerButtons.forEach(function (button) {
+    button.classList.remove("selected");
   });
 });
-
 
 // Aggiungi un evento al pulsante "Il bottone dei bottoni" per generare una nuova domanda
 document
@@ -217,77 +207,78 @@ document
   .addEventListener("click", function () {
     startTime();
     counter++;
-        counterElement.innerHTML = "Domanda :" + counter;
-    if (counter === 11){
-      //si va da marianna
+    counterElement.innerHTML = "Domanda :" + counter;
+    if (counter === 11) {
+      //PROCEDI A RESULTS PAGE E AGGIORNA RISULTATI
       document.getElementById("matteo").style.display = "none";
       document.getElementById("andrea").style.display = "none";
       document.getElementById("emanuele").style.display = "none";
-
       document.getElementById("marianna").style.display = "block";
 
       let totalQuestions = correctAnswers + wrongAnswers;
-      let correctPercentage = Math.round((correctAnswers / totalQuestions) * 100);
+      let correctPercentage = Math.round(
+        (correctAnswers / totalQuestions) * 100
+      );
       let wrongPercentage = Math.round((wrongAnswers / totalQuestions) * 100);
 
-      let numCorrectAnswersElement = document.getElementById("correctQuestions");
+      let numCorrectAnswersElement =
+        document.getElementById("correctQuestions");
       let numWrongAnswersElement = document.getElementById("wrongQuestions");
-  
-      numCorrectAnswersElement.innerHTML = correctAnswers  + "/10 questions";
-      numWrongAnswersElement.innerHTML = wrongAnswers  + "/10 questions";
-  
+
+      numCorrectAnswersElement.innerHTML = correctAnswers + "/10 questions";
+      numWrongAnswersElement.innerHTML = wrongAnswers + "/10 questions";
+
       // Aggiorna il contenuto degli elementi HTML che visualizzano le percentuali di risposte corrette e sbagliate
-      let correctPercentageElement = document.getElementById("correctPercentage");
+      let correctPercentageElement =
+        document.getElementById("correctPercentage");
       let wrongPercentageElement = document.getElementById("wrongPercentage");
-  
+
       correctPercentageElement.innerHTML = `${correctPercentage}%`;
       wrongPercentageElement.innerHTML = `${wrongPercentage}%`;
-  
+
       // Aggiorna il cerchio che mostra la percentuale di risposte corrette
       setProgress(correctPercentage);
-      
+
       // Visualizza un messaggio in base alla percentuale di risposte corrette
-      let topMessage = document.getElementById('topMessage');
-      let middleMessage = document.getElementById('middleMessage');
-      let underMessage = document.getElementById('underMessage');
-  
+      let topMessage = document.getElementById("topMessage");
+      let middleMessage = document.getElementById("middleMessage");
+      let underMessage = document.getElementById("underMessage");
+
       if (correctPercentage >= 60) {
-          topMessage.innerText = 'Congratulations!';
-          middleMessage.innerText = 'You passed the exam.';
-          underMessage.innerText = 'We\'ll send you the certificate in a few minutes. Check your email (including the promotions/spam folder)';
-        } else {
-          topMessage.innerText = 'We\'re sorry.';
-          middleMessage.innerText = 'You did not pass the exam.';
-          underMessage.innerText = 'Check your email (including the promotions/spam folder)';
+        topMessage.innerText = "Congratulations!";
+        middleMessage.innerText = "You passed the exam.";
+        underMessage.innerText =
+          "We'll send you the certificate in a few minutes. Check your email (including the     promotions/         spam folder)";
+      } else {
+        topMessage.innerText = "We're sorry.";
+        middleMessage.innerText = "You did not pass the exam.";
+        underMessage.innerText =
+          "Check your email (including the promotions/spam folder)";
       }
-    //   correctAnswersElement.innerText = correctAnswers; //serve per far displayare le risposte corrette da marianna
-    //   wrongAnswersElement.innerText = wrongAnswers; //uguale che a sopra
-    }else {
-      //senno si randomizza domanda
-      // Scegli una domanda casuale dall'array
+    } else {
+      // ALTRIMENTI GENERA NUOVA DOMANDA CASUALE
       let randomQuestion =
         questions[Math.floor(Math.random() * questions.length)];
-
-      // Aggiorna il testo della domanda
       questionElement.innerText = randomQuestion.question;
       questions.splice(
         questions.findIndex((i) => i.question === randomQuestion.question),
         1
       );
 
-        // Crea un array con tutte le risposte (giusta e sbagliate ps: si può fare solo in questo modo ) 
-        let answers = [...randomQuestion.wrongAnswers, randomQuestion.rightAnswer]
+      // Crea un array con tutte le risposte (giusta e sbagliate ps: si può fare solo in questo modo )
+      let answers = [
+        ...randomQuestion.wrongAnswers,
+        randomQuestion.rightAnswer,
+      ];
 
-        answerButtons.forEach((button, index) => {
-            button.innerText = answers[index]
-            button.dataset.correct = (answers[index] === randomQuestion.rightAnswer)
-            
-        });
+      answerButtons.forEach((button, index) => {
+        button.innerText = answers[index];
+        button.dataset.correct = answers[index] === randomQuestion.rightAnswer;
+      });
     }
   });
 
-//CERCHIO PERCENTUALE RISPOSTE MARI - SE FUNZIONASSE...
-
+//CERCHIO PERCENTUALE RESULTS PAGE
 var circle = document.querySelector("circle");
 var radius = circle.r.baseVal.value;
 var circumference = radius * 2 * Math.PI;
@@ -299,37 +290,28 @@ function setProgress(n) {
   const offset = circumference - (n / 100) * circumference;
   circle.style.strokeDashoffset = offset;
 }
-
-let correctPercentage = document.getElementById("correctPercentage");
-correctPercentage.innerText = `${correctAnswers} %`;
-let wrongPercentage = document.getElementById("wrongPercentage");
-wrongPercentage.innerText = `${wrongAnswers} %`;
-
-let numCorrectAnswersElement = document.getElementById("correctQuestions");
-let numWrongAnswersElement = document.getElementById("wrongQuestions");
-
-numCorrectAnswersElement.innerHTML = "Numero di risposte corrette: " + correctAnswers;
-numWrongAnswersElement.innerHTML = "Numero di risposte sbagliate: " + wrongAnswers;
-
-setProgress(correctAnswers*10);
+setProgress(correctAnswers * 10);
 
 if (correctAnswers < 101 && correctAnswers > 0) {
-    setProgress(correctAnswers);
+  setProgress(correctAnswers);
 }
 
-let topMessage = document.getElementById('topMessage');
-let middleMessage = document.getElementById('middleMessage');
-let underMessage = document.getElementById('underMessage');
+//DISPLAY MESSAGGIO RISULTATO ESAME
+let topMessage = document.getElementById("topMessage");
+let middleMessage = document.getElementById("middleMessage");
+let underMessage = document.getElementById("underMessage");
 
 function message() {
-    if (correctAnswers.length*10 >= 60) {
-        topMessage.innerText = 'Congratulations!';
-        middleMessage.innerText = 'You passed the exam.';
-        underMessage.innerText = 'We\'ll send you the certificate in a few minutes. Check your email (including the promotions/spam folder)'
-    } else {
-        topMessage.innerText = 'We\'re sorry.';
-        middleMessage.innerText = 'You did not pass the exam.';
-        underMessage.innerText = 'Check your email (including the promotions/spam folder)';
-    }
+  if (correctAnswers.length * 10 >= 60) {
+    topMessage.innerText = "Congratulations!";
+    middleMessage.innerText = "You passed the exam.";
+    underMessage.innerText =
+      "We'll send you the certificate in a few minutes. Check your email (including the promotions/spam folder)";
+  } else {
+    topMessage.innerText = "We're sorry.";
+    middleMessage.innerText = "You did not pass the exam.";
+    underMessage.innerText =
+      "Check your email (including the promotions/spam folder)";
+  }
 }
 message();
